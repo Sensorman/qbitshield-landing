f15d597854c77afdd86c99e9c9096d479304505e
Sensorman <wdaoudtt@gmail.com>
2025-04-07 11:57:25 -0400

    🚀 Fix layout to separate metadata from SupabaseProvider

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,4 @@
 // app/layout.js
-"use client";
-
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SupabaseProvider } from "./supabase-provider";
@@ -27,7 +25,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <SupabaseProvider>{children}</SupabaseProvider>
+        <SupabaseProvider>
+          {children}
+        </SupabaseProvider>
       </body>
     </html>
   );
