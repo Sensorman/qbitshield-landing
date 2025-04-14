7abcf2b798736ebf9f8227f6d7e9535dd3fe71e2
Sensorman <wdaoudtt@gmail.com>
2025-04-14 12:58:10 -0400

    chore: add TypeScript type dependencies

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -5,7 +5,7 @@ import { cookies } from 'next/headers'
 import type { NextRequest } from 'next/server'
 
 export async function updateSession(request: NextRequest): Promise<any> {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
