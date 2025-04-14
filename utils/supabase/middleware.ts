fd79e33d297f3ac1017ac651c3e618f3dd6be1d7
Sensorman <wdaoudtt@gmail.com>
2025-04-14 16:35:13 -0400

    debugging the cookies

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -6,7 +6,6 @@ import type { NextRequest } from 'next/server'
 
 export async function updateSession(request: NextRequest): Promise<any> {
   const cookieStore = await cookies(); // Await the cookies() function
-
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -16,18 +15,18 @@ export async function updateSession(request: NextRequest): Promise<any> {
           return cookieStore.get(name)?.value;
         },
         set(name, value, options) {
-          cookieStore.set({ name, value, ...options });
+          cookieStore.set({ name, value, ...options })
         },
         remove(name, options) {
-          cookieStore.set({ name, value: '', ...options });
+          cookieStore.set({ name, value: '', ...options })
         }
       }
     }
-  );
+  )
 
   const {
     data: { user }
-  } = await supabase.auth.getUser();
+  } = await supabase.auth.getUser()
 
-  return user;
+  return user
 }
