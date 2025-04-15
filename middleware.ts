fcbb2a5e789a57e4289c629238785494c94f64aa
Sensorman <wdaoudtt@gmail.com>
2025-04-14 20:16:44 -0400

    Fixing the cookies

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 // middleware.ts
 import { type NextRequest } from 'next/server'
-import { updateSession } from '@/utils/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
   return await updateSession(request)
