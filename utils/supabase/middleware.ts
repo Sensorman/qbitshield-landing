d4e85c181eb387aff53210c0a9005453451aad73
Sensorman <wdaoudtt@gmail.com>
2025-04-14 20:20:13 -0400

    Fixing the cookies

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -5,16 +5,12 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next()
   const supabase = createMiddlewareClient({ req: request, res: response })
+
   await supabase.auth.getSession()
+
   return response
 }
 
 export const config = {
-  matcher: [
-    /*
-      Match routes you want middleware to protect
-      Example: only protect /dashboard
-    */
-    '/dashboard/:path*',
-  ],
+  matcher: ['/dashboard/:path*'],
 }
